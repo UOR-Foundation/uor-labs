519b9e4a5f8144794ee79250cc3e796a95c9cc39
humuhumu33 <68450279+humuhumu33@users.noreply.github.com>
2025-05-25 23:03:53 +0100

    Add tests for assembler jumps and prime factor caching

--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -26,6 +26,43 @@ class AssemblerTest(unittest.TestCase):
         out = ''.join(VM().execute(prog))
         self.assertEqual(out, '321')
 
+    def test_numeric_negative_jump(self):
+        src = """
+        PUSH 3
+        STORE 0
+        LOAD 0
+        PRINT
+        LOAD 0
+        PUSH 1
+        SUB
+        STORE 0
+        LOAD 0
+        JNZ -8
+        """
+        prog = assembler.assemble(src)
+        out = ''.join(VM().execute(prog))
+        self.assertEqual(out, '321')
+
+    def test_label_resolution_forward(self):
+        src = """
+        JMP end
+        PUSH 1
+        end:
+        PUSH 2
+        PRINT
+        """
+        prog = assembler.assemble(src)
+        out = ''.join(VM().execute(prog))
+        self.assertEqual(out, '2')
+
+    def test_unknown_label_error(self):
+        with self.assertRaises(ValueError):
+            assembler.assemble("JMP nowhere")
+
+    def test_invalid_instruction_error(self):
+        with self.assertRaises(ValueError):
+            assembler.assemble("FOO 1")
+
 
 if __name__ == '__main__':
     unittest.main()
