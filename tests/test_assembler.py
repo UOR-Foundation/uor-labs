a87bb4b349abf252e9dd21c9c2fa3aaed2c2f9bc
humuhumu33 <68450279+humuhumu33@users.noreply.github.com>
2025-05-26 20:18:18 +0100

    Extend assembler with new opcodes

--- tests/test_assembler.py
+++ tests/test_assembler.py
@@ -77,6 +77,37 @@ class AssemblerTest(unittest.TestCase):
             with self.subTest(op=asm):
                 self.assertEqual(assembler.assemble(asm), [expected])
 
+    def test_extended_opcodes(self):
+        cases = [
+            ("CALL 3", chunks.chunk_call(3)),
+            ("RET", chunks.chunk_ret()),
+            ("ALLOC 8", chunks.chunk_alloc(8)),
+            ("FREE 5", chunks.chunk_free(5)),
+            ("INPUT", chunks.chunk_input()),
+            ("OUTPUT", chunks.chunk_output()),
+            ("NET_SEND", chunks.chunk_net_send()),
+            ("NET_RECV", chunks.chunk_net_recv()),
+            ("THREAD_START", chunks.chunk_thread_start()),
+            ("THREAD_JOIN", chunks.chunk_thread_join()),
+        ]
+        for asm, expected in cases:
+            with self.subTest(op=asm):
+                self.assertEqual(assembler.assemble(asm), [expected])
+
+    def test_call_negative_offset(self):
+        src = """
+        PUSH 1
+        CALL -1
+        RET
+        """
+        prog = assembler.assemble(src)
+        expected = [
+            chunks.chunk_push(1),
+            chunks.chunk_call(-1),
+            chunks.chunk_ret(),
+        ]
+        self.assertEqual(prog, expected)
+
 
 if __name__ == '__main__':
     unittest.main()
